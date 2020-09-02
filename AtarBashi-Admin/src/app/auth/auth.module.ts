ff8172aecf49e80790b6a56d4f543c10a24feea3
MohammadRidha <60443137+MohammadRidha@users.noreply.github.com>
2020-09-02 18:51:51 +0430

    create login

--- AtarBashi-Admin/src/app/auth/auth.module.ts
+++ AtarBashi-Admin/src/app/auth/auth.module.ts
@@ -6,17 +6,23 @@ import { authRoutes } from './routes/routes';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(authRoutes)
+    RouterModule.forRoot(authRoutes),
+    FormsModule,
+    HttpClientModule
   ],
   declarations: [
     AuthComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    providers: [AuthService],
   ]
 })
 export class AuthModule { }
